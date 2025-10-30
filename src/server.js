925e16c6c0a669cdc4bfec9b78056fc1f386c0c2
Lina <linamark0001@gmail.com>
2025-10-30 13:34:53 +0200

    fix3

--- src/server.js
+++ src/server.js
@@ -31,6 +31,11 @@ app.use(authRoutes);
 app.use(notesRoutes);
 app.use(userRoutes);
 
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Server is alive ✅" });
+});
+
 app.use(notFoundHandler);
 app.use(celebrateErrors());
 app.use(errorHandler);
@@ -48,6 +53,7 @@ const PORT = Number(process.env.PORT) || 3030;
     process.exit(1);
   }
 })();
+
 
 
 
